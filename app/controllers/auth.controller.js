678f6a05b907c8cfb3ce7f5665b81b6348da47dd
Mikita Vydrankou <mikitavydrankou@gmail.com>
2025-04-22 14:30:00 +0200

    feature | switch to vite, added react-query, zustand

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -9,102 +9,103 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 export const signup = (req, res) => {
-  // user role = 1
-  // admin role = 2
-  // moderator role = 3
+    // user role = 1
+    // admin role = 2
+    // moderator role = 3
 
-  const roleName = req.body.role || "user";
+    const roleName = req.body.role || "user";
+    console.log(roleName);
 
-  Role.findOne({
-    where: {
-      name: roleName,
-    },
-  })
-    .then((role) => {
-      if (!role) {
-        return Role.findOne({ where: { name: "user" } });
-      }
-      return role;
+    Role.findOne({
+        where: {
+            name: roleName,
+        },
     })
-    .then((role) => {
-      return User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 8),
-        roleId: role.id,
-        link: req.body.link,
-      });
-    })
-    .then((user) => {
-      const token = jwt.sign({ id: user.id }, config.secret, {
-        algorithm: "HS256",
-        allowInsecureKeySizes: true,
-        expiresIn: 86400,
-      });
+        .then((role) => {
+            if (!role) {
+                return Role.findOne({ where: { name: "user" } });
+            }
+            return role;
+        })
+        .then((role) => {
+            return User.create({
+                username: req.body.username,
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password, 8),
+                roleId: role.id,
+                link: req.body.link,
+            });
+        })
+        .then((user) => {
+            const token = jwt.sign({ id: user.id }, config.secret, {
+                algorithm: "HS256",
+                allowInsecureKeySizes: true,
+                expiresIn: 86400,
+            });
 
-      return Role.findByPk(user.roleId).then((role) => {
-        const roleName = role ? role.name : "user";
-        const authorities = ["ROLE_" + roleName.toUpperCase()];
+            return Role.findByPk(user.roleId).then((role) => {
+                const roleName = role ? role.name : "user";
+                const authorities = roleName;
 
-        res.status(200).send({
-          message: "User registered successfully!",
-          id: user.id,
-          username: user.username,
-          email: user.email,
-          roles: authorities,
-          link: user.link,
-          accessToken: token,
+                res.status(200).send({
+                    message: "User registered successfully!",
+                    id: user.id,
+                    username: user.username,
+                    email: user.email,
+                    role: authorities,
+                    link: user.link,
+                    accessToken: token,
+                });
+            });
+        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message });
         });
-      });
-    })
-    .catch((err) => {
-      res.status(500).send({ message: err.message });
-    });
 };
 
 export const signin = (req, res) => {
-  User.findOne({
-    where: {
-      username: req.body.username,
-    },
-    include: Role,
-  })
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send({ message: "User Not found." });
-      }
-
-      const passwordIsValid = bcrypt.compareSync(
-        req.body.password,
-        user.password
-      );
-
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          accessToken: null,
-          message: "Invalid Password!",
-        });
-      }
-
-      const token = jwt.sign({ id: user.id }, config.secret, {
-        algorithm: "HS256",
-        allowInsecureKeySizes: true,
-        expiresIn: 86400, // 24 h
-      });
-
-      const roleName = user.Role ? user.Role.name : "user";
-      const authorities = ["ROLE_" + roleName.toUpperCase()];
-
-      res.status(200).send({
-        id: user.id,
-        username: user.username,
-        email: user.email,
-        roles: authorities,
-        link: user.link,
-        accessToken: token,
-      });
+    User.findOne({
+        where: {
+            username: req.body.username,
+        },
+        include: Role,
     })
-    .catch((err) => {
-      res.status(500).send({ message: err.message });
-    });
+        .then((user) => {
+            if (!user) {
+                return res.status(404).send({ message: "User Not found." });
+            }
+
+            const passwordIsValid = bcrypt.compareSync(
+                req.body.password,
+                user.password
+            );
+
+            if (!passwordIsValid) {
+                return res.status(401).send({
+                    accessToken: null,
+                    message: "Invalid Password!",
+                });
+            }
+
+            const token = jwt.sign({ id: user.id }, config.secret, {
+                algorithm: "HS256",
+                allowInsecureKeySizes: true,
+                expiresIn: 86400, // 24 h
+            });
+
+            const roleName = user.Role ? user.Role.name : "user";
+            const authorities = roleName;
+
+            res.status(200).send({
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                role: authorities,
+                link: user.link,
+                accessToken: token,
+            });
+        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message });
+        });
 };
--- app/middleware/verifySignUp.js
+++ app/middleware/verifySignUp.js
@@ -1,63 +1,57 @@
 import db from "../models/index.js";
 
-const ROLES = db.ROLES;
+const Role = db.Role;
 const User = db.User;
 
 const checkDuplicateUsernameOrEmail = (req, res, next) => {
-  User.findOne({
-    where: {
-      username: req.body.username,
-    },
-  }).then((user) => {
-    if (user) {
-      res.status(400).send({
-        message: "Failed! Username is already in use!",
-      });
-      return;
-    }
-
-    User.findOne({
-      where: {
-        email: req.body.email,
-      },
-    }).then((user) => {
-      if (user) {
-        res.status(400).send({
-          message: "Failed! Email is already in use!",
+    Promise.all([
+        User.findOne({ where: { username: req.body.username } }),
+        User.findOne({ where: { email: req.body.email } }),
+    ])
+        .then(([userByUsername, userByEmail]) => {
+            if (userByUsername) {
+                return res
+                    .status(400)
+                    .send({ message: "Failed! Username is already in use!" });
+            }
+            if (userByEmail) {
+                return res
+                    .status(400)
+                    .send({ message: "Failed! Email is already in use!" });
+            }
+            next();
+        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message });
         });
-        return;
-      }
-      next();
-    });
-  });
 };
 
 const checkRolesExisted = (req, res, next) => {
-  if (req.body.role) {
-    Role.findOne({
-      where: {
-        name: req.body.role,
-      },
-    })
-      .then((role) => {
-        if (!role) {
-          return res.status(400).send({
-            message: `Role '${req.body.role}' doesn't exist!`,
-          });
-        }
+    if (req.body.role) {
+        Role.findOne({
+            where: {
+                name: req.body.role,
+            },
+        })
+            .then((role) => {
+                if (!role) {
+                    return res.status(400).send({
+                        message: `Role '${req.body.role}' doesn't exist!`,
+                    });
+                }
+                next();
+            })
+            .catch((err) => {
+                res.status(500).send({ message: err.message });
+            });
+    } else {
         next();
-      })
-      .catch((err) => {
-        res.status(500).send({ message: err.message });
-      });
-  } else {
-    next();
-  }
+    }
 };
 
 const verifySignUp = {
-  checkDuplicateUsernameOrEmail: checkDuplicateUsernameOrEmail,
-  checkRolesExisted: checkRolesExisted,
+    checkDuplicateUsernameOrEmail: checkDuplicateUsernameOrEmail,
+    checkRolesExisted: checkRolesExisted,
 };
 
 export default verifySignUp;
