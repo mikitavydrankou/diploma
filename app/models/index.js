568380011c146a19f074dff807b910aebfd2445c
Mikita Vydrankou <mikitavydrankou@gmail.com>
2025-05-05 23:35:10 +0200

    feature | we are on vite + design

--- app/models/index.js
+++ app/models/index.js
@@ -9,6 +9,7 @@ const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
     host: config.HOST,
     dialect: config.dialect,
     logging: false,
+    timezone: "+02:00",
 });
 
 try {
