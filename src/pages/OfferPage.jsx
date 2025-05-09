982c44f7f3e48f514b5ee56b8f6d899365b07afc
Mikita Vydrankou <mikitavydrankou@gmail.com>
2025-05-09 14:44:55 +0200

    fix

--- src/pages/OfferPage.jsx
+++ src/pages/OfferPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useOfferById } from "../api/offerQueries";
-import BackButton from "../components/Buttons/BackButton";
+import BackButton from "../components/buttons/BackButton";
 import { DeleteButton } from "../components/Buttons/DeleteOfferButton";
 import { useAuthStore } from "../store/authStore";
 import {
