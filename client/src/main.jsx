2347b1190eae51c4a1253b8119068b3fce5d5ea5
Mikita Vydrankou <mikitavydrankou@gmail.com>
2025-05-12 19:22:02 +0200

    minor

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,18 +3,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { queryClient } from "./api/queryClient.js";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 
 createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
-            <Router>
+            <HashRouter>
                 <StrictMode>
                     <App />
                 </StrictMode>
-            </Router>
+            </HashRouter>
         </ThemeProvider>
     </QueryClientProvider>
 );
